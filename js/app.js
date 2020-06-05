14702b3b9a022086324ad182819689a3e5b07847
Shan <itsshankart44@gmail.com>
2020-06-05 19:36:00 +0530

    Final layout

---
+++ js/app.js
@@ -0,0 +1,20 @@
+// AOS.init();
+
+// (function($) {
+//     $(document).ready(function(){
+//         $('#mynav').hide();
+    
+//         $(function(){
+//             $(window).scroll(function(){
+//                 if ($(this).scrollTop() > 600){
+//                     $('#mynav').fadeIn(500);
+//                 } else {
+//                     $('#mynav').fadeOut(500);
+//                 }
+//             });
+//         });
+             
+//     });
+//     })(jQuery);   
+
+
